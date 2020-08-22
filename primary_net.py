0871782036ed084e382c90af41a98eca01da1872
Shresth verma <vermashresth@gmail.com>
2020-08-23 00:43:19 +0530

    Update primary_net.py

--- primary_net.py
+++ primary_net.py
@@ -18,17 +18,18 @@ class Embedding(nn.Module):
 
         h,k = self.z_num
 
-        for i in range(h):
-            for j in range(k):
-                self.z_list.append(Parameter(torch.fmod(torch.randn(self.z_dim).cuda(), 2)))
+        # for i in range(h):
+        #     for j in range(k):
+        #         self.z_list.append(Parameter(torch.fmod(torch.randn(self.z_dim).cuda(), 2)))
 
-    def forward(self, hyper_net):
+    def forward(self, hyper_net, z_list):
+        # print("shape", z_list.shape)
         ww = []
         h, k = self.z_num
         for i in range(h):
             w = []
             for j in range(k):
-                w.append(hyper_net(self.z_list[i*k + j]))
+                w.append(hyper_net(z_list[i*k + j]))
             ww.append(torch.cat(w, dim=1))
         return torch.cat(ww, dim=0)
 
@@ -43,7 +44,7 @@ class PrimaryNetwork(nn.Module):
         self.z_dim = z_dim
         self.hope = HyperNetwork(z_dim=self.z_dim)
 
-        self.zs_size = [[1, 1], [1, 1], [2,1],[2,2], [2,2],[2,2],[4,2], [4, 4]]
+        self.zs_size = [[1, 1], [1, 1], [2,1],[2,2], [2,2],[2,2],[4,2], [4, 4], [1,1],[1,1]]
 
         self.filter_size = [[16,16], [16,32], [32,32], [32,64]]
 
@@ -53,15 +54,27 @@ class PrimaryNetwork(nn.Module):
             down_sample = False
             if i in [1,3]:
                 down_sample = True
-            self.res_net.append(ResNetBlock(self.filter_size[i][0], self.filter_size[i][1], downsample=down_sample))
+            self.res_net.append(ResNetBlock(self.filter_size[i][0], self.filter_size[i][1], downsample=down_sample, my=self.zs_size[2*(i+1)], my1=self.zs_size[2*(i+1)+1]))
 
         self.zs = nn.ModuleList()
 
         for i in range(8):
             self.zs.append(Embedding(self.zs_size[i], self.z_dim))
+        
+        self.break_size = []
+        
+        for i in range(5):
+          a0=self.zs_size[2*i][0]
+          a1=self.zs_size[2*i][1]
+          b0=self.zs_size[2*i+1][0]
+          b1=self.zs_size[2*i+1][1]
+          self.break_size.append([a0,a1,b0,b1])
 
         self.global_avg = nn.AvgPool2d(8)
         self.final = nn.Linear(64,10)
+
+        self.z_list0 = [Parameter(torch.fmod(torch.randn(self.z_dim).cuda(), 2))]
+        self.z_list1 = [Parameter(torch.fmod(torch.randn(self.z_dim).cuda(), 2))]
 
     def forward(self, x):
 
@@ -69,9 +82,15 @@ class PrimaryNetwork(nn.Module):
 
         for i in range(4):
             # if i != 15 and i != 17:
-            w1 = self.zs[2*i](self.hope)
-            w2 = self.zs[2*i+1](self.hope)
-            x= self.res_net[i](x, w1, w2)
+            print(i)
+            w1 = self.zs[2*i](self.hope, self.z_list0)
+            w2 = self.zs[2*i+1](self.hope, self.z_list1)
+            x, message = self.res_net[i](x, w1, w2)
+            print(message.shape)
+            a0,a1,b0, b1 = self.break_size[i+1]
+            z_list0, z_list1 = torch.split(message, [a0*a1*64,b0*b1*64], dim=-1)
+            self.z_list0 = z_list0.view(-1, a0, a1, 64)
+            self.z_list1 = z_list1.view(-1, b0, b1, 64)
             # print("resblock ", i, "shape ", x.shape)
         x = self.global_avg(x)
         x = self.final(x.view(-1,64))
